3dfe7c07e7a9744e76a6c3830e539cb825686427
Orlando Marques <realorlanditomadrid@gmail.com>
2022-02-26 11:00:22 +0100

    ADD: process endpoint

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var validator = require('xsd-schema-validator');
+var fs = require('fs');
 
 router.post('/check', function(req, res, next) {
     try{
@@ -22,6 +23,37 @@ router.post('/check', function(req, res, next) {
         res.json({
             "result": "Error 500",
             "valid": null,
+            "messages": [error.message]
+        });
+    }
+
+});
+
+router.post('/process', function(req, res, next) {
+    try{
+        let xmlStr = req.rawBody;
+        let directoryPath = 'sitioWeb/resultados';
+        fs.writeFile(`${directoryPath}/result.xml`,xmlStr,(err,result)=>{
+            if(err && !result)
+                res.json({
+                    "result": "Error 500",
+                    "valid": null,
+                    "messages": [err.message]
+                });
+            else{
+                res.json({
+                    "result": "ok",
+                    "success": true,
+                    "messages": [],
+                    ...result
+                });
+            }
+        });
+    }catch (error) {
+        res.statusCode = 500;
+        res.json({
+            "result": "Error 500",
+            "success": false,
             "messages": [error.message]
         });
     }
