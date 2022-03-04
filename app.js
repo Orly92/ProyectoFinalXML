337702f5d2e10fe2f4e54d3a9ad12aa1fdcb7dfb
Orlando Marques <realorlanditomadrid@gmail.com>
2022-03-04 19:36:35 +0100

    ADD: cors to api

--- app.js
+++ app.js
@@ -4,22 +4,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var xmlparser = require('express-xml-bodyparser');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(xmlparser());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
